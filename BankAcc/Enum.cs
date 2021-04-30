d57c4f74954f06dd2cf6cbb0d60c6c95855aac1a
Ksssuch <ksssuch@mail.ru>
2021-04-30 22:57:42 +0300

    my firs commit

---
+++ BankAcc/Enum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAcc
+{
+
+    public enum AccountType { Checking, Deposit }
+    class Enum
+    {
+        static void Main(string[] args)
+        {
+            AccountType goldAccount;
+            AccountType platinumAccount;
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
+            Console.WriteLine("The custumer Account Type is {0}", goldAccount);
+            Console.WriteLine("The custumer Account Type is {0}", platinumAccount);
+
+
+        }
+    }
+}
